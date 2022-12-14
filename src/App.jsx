7d94ecc68466c1857db23ba71fdf42ff0f6b357d
keyy123 <parktwins1@gmail.com>
2022-12-13 22:27:05 -0500

    add pre-existing local files to repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <div>
+            <h2>Hello</h2>
+            <p>You have changed...</p>
+        </div>
+    )
+}
+
+const container = document.getElementById("root");
+ReactDOM.createRoot(container).render(<App/>);
