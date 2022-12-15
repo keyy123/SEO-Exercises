29e1e4e18dc4c12e9bcb7f823735641c3a24cdb9
keyy123 <parktwins1@gmail.com>
2022-12-15 18:36:07 -0500

    feature: add structured data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-    return (
-        <div>
-            <h2>Hello</h2>
-            <p>You have changed...</p>
-            <img src="/SEO.png" alt="Search Engine Optimization or SEO"/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Hello</h2>
+      <p>You have changed...</p>
+      <img src="/SEO.png" alt="Search Engine Optimization or SEO" />
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
-ReactDOM.createRoot(container).render(<App/>);
+ReactDOM.createRoot(container).render(<App />);
